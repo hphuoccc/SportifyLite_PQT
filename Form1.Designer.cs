93b04cdd76dcfdb0d845e47d8a4596bd6c55091b
EngWang <138361071+EngWang@users.noreply.github.com>
2024-04-18 14:41:08 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace showIn4
+{
+    partial class In4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.Label();
+            this.gender = new System.Windows.Forms.Label();
+            this.birth = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(389, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "THÔNG TIN NGƯỜI DÙNG";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.name.Location = new System.Drawing.Point(28, 93);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(70, 26);
+            this.name.TabIndex = 1;
+            this.name.Text = "label2";
+            // 
+            // id
+            // 
+            this.id.AutoSize = true;
+            this.id.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.id.Location = new System.Drawing.Point(28, 144);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(70, 26);
+            this.id.TabIndex = 2;
+            this.id.Text = "label2";
+            // 
+            // gender
+            // 
+            this.gender.AutoSize = true;
+            this.gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.gender.Location = new System.Drawing.Point(28, 194);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(70, 26);
+            this.gender.TabIndex = 3;
+            this.gender.Text = "label2";
+            // 
+            // birth
+            // 
+            this.birth.AutoSize = true;
+            this.birth.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.birth.Location = new System.Drawing.Point(28, 245);
+            this.birth.Name = "birth";
+            this.birth.Size = new System.Drawing.Size(70, 26);
+            this.birth.TabIndex = 4;
+            this.birth.Text = "label2";
+            // 
+            // In4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 645);
+            this.Controls.Add(this.birth);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label1);
+            this.Name = "In4";
+            this.Text = "Thông tin";
+            this.Load += new System.EventHandler(this.In4_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.Label gender;
+        private System.Windows.Forms.Label birth;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Firebase.Database;
+using Firebase.Database.Query;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace showIn4
+{
+    public partial class In4 : Form
+    {
+        IFirebaseClient client;
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "CkIjEA3D7s7dxH1BWr0WF4VcU0ab7M068ojHUlDG",
+            BasePath = "https://loginandregister-f73c6-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        };
+
+        public In4()
+        {
+            InitializeComponent();
+        }
+        
+        private async void In4_Load(object sender, EventArgs e)     
+        {
+            // Khởi tạo kết nối Firebase
+            client = new FireSharp.FirebaseClient(config);
+
+            // Lấy dữ liệu từ Firebase
+            FirebaseResponse response = await client.GetAsync("users/1"); // Thay đổi đường dẫn "users/1" bằng đường dẫn thực tế của dữ liệu bạn muốn lấy
+
+            // Kiểm tra phản hồi từ Firebase
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                // Parse dữ liệu từ phản hồi
+                User user = response.ResultAs<User>(); // Giả sử User là một lớp đại diện cho dữ liệu người dùng
+
+                // Hiển thị thông tin người dùng trên các thành phần giao diện người dùng
+                name.Text = user.realname;
+                id.Text = user.userid.ToString();
+                birth.Text = user.birthday.ToString();
+                gender.Text = user.gender;
+            }
+            else
+            {
+                // Xử lý trường hợp không thành công khi lấy dữ liệu từ Firebase
+                MessageBox.Show("Failed to retrieve data from Firebase.");
+            }
+        }
+    }
+  
+}
